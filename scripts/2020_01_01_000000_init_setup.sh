c295f8dbbdb7d99be2fe747fb4c16c63c5a373a0
Christian D'Aquino <christian.daquino@gmail.com>
2022-07-03 21:07:19 +0800

    d

--- scripts/2020_01_01_000000_init_setup.sh
+++ scripts/2020_01_01_000000_init_setup.sh
@@ -2,11 +2,10 @@
 
 #Ubuntu Deployment
 echo "Ubuntu Updates"
+sudo add-apt-repository ppa:ondrej/php -y
+
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt install curl ca-certificates apt-transport-https software-properties-common \
 gnupg lsb-release unzip smbclient \
 -y
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
-sudo apt-get -y upgrade
--- scripts/2020_01_01_000007_install_redis.sh
+++ scripts/2020_01_01_000007_install_redis.sh
@@ -3,5 +3,5 @@
 #Redis
 echo "Installing Redis"
 sudo apt install redis-server -y
-sudo systemctl restart redis.service
+# sudo systemctl restart redis.service
 # sudo systemctl status redis
