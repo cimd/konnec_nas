3cb9c877b9ff88c8f07d1a1abf468a729d44fdf3
Christian D'Aquino <christian.daquino@gmail.com>
2022-07-04 22:17:19 +0800

    Update install.php

--- app/Console/Commands/Packages/Cockpit/install.php
+++ app/Console/Commands/Packages/Cockpit/install.php
@@ -43,7 +43,7 @@ class install extends Command
     {
         // $package = Package::where('name', 'Webmin')->first();
 
-        $process = new Process([app_path('Console/Commands/Packages/Cockpit/install.sh')]);
+        $process = new Process(['./app/Console/Commands/Packages/Cockpit/install.sh']);
         $process->setTimeout(120);
         $process->start();
 
