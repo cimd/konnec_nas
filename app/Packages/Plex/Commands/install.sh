4145bba4b1fd123cc7cee899ea3d409193af145a
Christian D'Aquino <christian.daquino@gmail.com>
2022-07-06 22:30:21 +0800

    Update install.sh

--- app/Packages/Plex/Commands/install.sh
+++ app/Packages/Plex/Commands/install.sh
@@ -3,5 +3,5 @@
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | tee /etc/apt/sources.list.d/plexmediaserver.list
 apt update -y
-apt install plexmediaserver -y
+yes | apt install plexmediaserver -y
 ufw allow 32400
