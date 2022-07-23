26787f5d411cadabf83ee567eb29128a43613187
Christian D'Aquino <christian.daquino@gmail.com>
2022-07-23 21:05:23 +0800

    d

--- scripts/2020_01_01_000000_init_setup.sh
+++ scripts/2020_01_01_000000_init_setup.sh
@@ -2,9 +2,9 @@
 
 #Ubuntu Deployment
 echo "Ubuntu Updates"
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt update
+sudo apt -y upgrade
 sudo apt install curl ca-certificates apt-transport-https software-properties-common \
-gnupg lsb-release unzip smbclient \
+gnupg lsb-release unzip smbclient \ git
 -y
-sudo apt-get update
+sudo apt update
--- scripts/old/virtualbox.sh
+++
@@ -1,2 +0,0 @@
-wget https://download.virtualbox.org/virtualbox/6.1.24/Oracle_VM_VirtualBox_Extension_Pack-6.1.24.vbox-extpack
-sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.24.vbox-extpack
