0b5f13768a6d55dc243785d00dd7476aa293b894
Shift <shift@laravelshift.com>
2023-05-26 11:48:36 +0000

    Adopt anonymous migrations

--- database/migrations/2021_06_26_032939_create_cfg_scripts_table.php
+++ database/migrations/2021_06_26_032939_create_cfg_scripts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCfgScriptsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreateCfgScriptsTable extends Migration
     {
         Schema::dropIfExists('cfg_scripts');
     }
-}
+};
