660bd117300ed3766a610b44034a8264e746ed6f
Christian D'Aquino <christian.daquino@gmail.com>
2024-07-27 20:47:02 +0800

    d

--- scripts/2020_01_01_000000_init_setup.sh
+++ scripts/2020_01_01_000000_init_setup.sh
@@ -5,6 +5,6 @@ echo "Ubuntu Updates"
 sudo apt update
 sudo apt -y upgrade
 sudo apt install curl ca-certificates apt-transport-https software-properties-common \
-gnupg lsb-release unzip smbclient \ git
--y
+gnupg lsb-release unzip smbclient \
+git -y
 sudo apt update
---
+++ scripts/2020_01_01_0000010_install_container_portainer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing Container - Portainer"
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
